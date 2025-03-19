6f0b2d863f5be83fac82a3a2e8a50357ee538de0
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2025-03-19 22:32:38 +0100

    Changed color of 'change view' button

---
+++ Assets/Scripts/NoteManagerScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class NoteManagerScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    //Color palette:
+    //https://coolors.co/8bc348-f5c523-fe5377-0b3954-bfd7ea
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
