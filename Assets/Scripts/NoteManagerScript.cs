e8284f032778d3393e7655e013061062f7eda205
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2025-03-20 16:51:40 +0100

    Added keybind functions to switch with tab

--- Assets/Scripts/NoteManagerScript.cs
+++ Assets/Scripts/NoteManagerScript.cs
@@ -1,8 +1,15 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NoteManagerScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Input Fields")]
+    public List<TMP_InputField> lst_InputFields;
+
+    private bool isTabPressed = false;
 
     //Color palette:
     //https://coolors.co/8bc348-f5c523-fe5377-0b3954-bfd7ea
@@ -14,6 +21,37 @@ public class NoteManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SelectOtherInputField();
+    }
+
+    private void SelectOtherInputField()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && !isTabPressed)
+        {
+            isTabPressed = true;
+
+            if (lst_InputFields.Count > 0)
+            {
+                for (int i = 0; i < lst_InputFields.Count; i++)
+                {
+                    if (lst_InputFields[i].isFocused)
+                    {
+                        if (i == lst_InputFields.Count - 1)
+                        {
+                            lst_InputFields[0].Select();
+                        }
+                        else
+                        {
+                            lst_InputFields[i + 1].Select();
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            isTabPressed = false;
+        }
     }
 }
