d969251ca2209e21c82f84b598b3ff15c1afbe75
AlirizaSari <AlisSari55@hotmail.com>
2025-03-24 15:27:30 +0100

    Added Models
    - ParentGuardian
    - Patient
    - Doctor
    - Traject
    - CareMoment

---
+++ Assets/Scripts/Models/CareMoment.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class CareMoment
+{
+    public string id;
+
+    public string name;
+
+    public string url;
+
+    public byte[] Picture;
+
+    public int timeDurationInMin;
+}
---
+++ Assets/Scripts/Models/ParentGuardian.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class ParentGuardian
+{
+    public string id;
+
+    public string UserId;
+
+    public string FirstName;
+
+    public string LastName;
+}
