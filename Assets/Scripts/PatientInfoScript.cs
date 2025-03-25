0c4613699bddfe17d520efe8393421f466b71787
MathijsH6 <mathijs.haest@ziggo.nl>
2025-03-25 13:31:35 +0100

    PatientInfoPagina
    ben heeeeeel misschien weer vergeten om meerdere commits tedoen (gelukkig is er niet heel veel in een keer)

---
+++ Assets/Scripts/PatientInfoScript.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PatientInfoScript : MonoBehaviour
+{
+    public TMP_InputField NameInput;
+    public TMP_InputField BirthdayInput;
+    public TMP_InputField DoctorInput;
+    public TMP_InputField DateInput;
+    public TMP_InputField RoutePlanInput;
+
+    public Button SaveInfoButton;
+}
