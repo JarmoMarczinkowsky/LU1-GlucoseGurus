6f8e433d5b7f1abc1df44b2e4cfa4ec76ed3357c
Reinout <94532201+ReinoutvdG@users.noreply.github.com>
2025-03-25 01:22:37 +0100

    apiclient toegevoegd
    als environments en objects er uit dorden gehaald in models kan unity niet meer opstarten dus moet later zorgvuldiger er uit worden gehaald
    (zelf kijken of je het naar main wil pushen)

---
+++ Assets/Code/ApiClient/ModelApiClients/UserApiClient.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class UserApiClient : MonoBehaviour
+{
+    public WebClient webClient;
+    public static UserApiClient Singleton;
+
+    private void Awake()
+    {
+        if (Singleton == null)
+        {
+            Singleton = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject); // Prevent duplicate instances
+            Debug.Log("Destroyed duplicate ExampleApp instance");
+        }
+    }
+    public async Awaitable<IWebRequestReponse> Register(User user)
+    {
+        string route = "/account/register";
+        string data = JsonUtility.ToJson(user);
+
+        return await webClient.SendPostRequest(route, data);
+    }
+
+    public async Awaitable<IWebRequestReponse> Login(User user)
+    {
+        string json = JsonUtility.ToJson(user);
+        var response = await webClient.SendPostRequest("/account/login", json);
+
+        if (response is WebRequestData<Token> tokenResponse)
+        {
+            Debug.Log("Login successful, saving token: " + tokenResponse.Data.accessToken);
+
+            webClient.SetToken(tokenResponse.Data.accessToken);
+            PlayerPrefs.SetString("AuthToken", tokenResponse.Data.accessToken); // Optioneel: token opslaan
+            PlayerPrefs.Save();
+
+            return new WebRequestData<string>("Success");
+        }
+
+        return response;
+    }
+
+
+    private IWebRequestReponse ProcessLoginResponse(IWebRequestReponse webRequestResponse)
+    {
+        switch (webRequestResponse)
+        {
+            case WebRequestData<string> data:
+                Debug.Log("Response data raw: " + data.Data);
+                string token = JsonHelper.ExtractToken(data.Data);
+                webClient.SetToken(token);
+                return new WebRequestData<string>("Succes");
+            default:
+                return webRequestResponse;
+        }
+    }
+
+}
+
---
+++ Assets/Code/ApiClient/WebClient.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebClient : MonoBehaviour
+{
+    public string baseUrl;
+    private string token;
+
+    public static WebClient Singleton;
+
+    private void Awake()
+    {
+        if (Singleton == null)
+        {
+            Singleton = this;
+            DontDestroyOnLoad(this.gameObject);
+
+            // Haal token op uit PlayerPrefs
+            if (PlayerPrefs.HasKey("AuthToken"))
+            {
+                string savedToken = PlayerPrefs.GetString("AuthToken");
+                SetToken(savedToken);
+                Debug.Log("Loaded saved token.");
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetToken(string token)
+    {
+        this.token = token;
+    }
+
+    public async Awaitable<IWebRequestReponse> SendGetRequest(string route)
+    {
+        UnityWebRequest webRequest = CreateWebRequest("GET", route, "");
+        return await SendWebRequest(webRequest);
+    }
+
+    public async Awaitable<IWebRequestReponse> SendPostRequest(string route, string data)
+    {
+        UnityWebRequest webRequest = CreateWebRequest("POST", route, data);
+        IWebRequestReponse response = await SendWebRequest(webRequest);
+        return response;
+    }
+
+    public async Awaitable<IWebRequestReponse> SendPutRequest(string route, string data)
+    {
+        UnityWebRequest webRequest = CreateWebRequest("PUT", route, data);
+        return await SendWebRequest(webRequest);
+    }
+
+    public async Awaitable<IWebRequestReponse> SendDeleteRequest(string route)
+    {
+        UnityWebRequest webRequest = CreateWebRequest("DELETE", route, "");
+        return await SendWebRequest(webRequest);
+    }
+
+    private UnityWebRequest CreateWebRequest(string type, string route, string data)
+    {
+        string url = baseUrl + route;
+        Debug.Log("Creating " + type + " request to " + url + " with data: " + data);
+
+        data = RemoveIdFromJson(data); // Backend throws error if it receiving empty strings as a GUID value.
+        var webRequest = new UnityWebRequest(url, type);
+        byte[] dataInBytes = new UTF8Encoding().GetBytes(data);
+        webRequest.uploadHandler = new UploadHandlerRaw(dataInBytes);
+        webRequest.downloadHandler = new DownloadHandlerBuffer();
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+        AddToken(webRequest);
+        return webRequest;
+    }
+
+    private async Awaitable<IWebRequestReponse> SendWebRequest(UnityWebRequest webRequest)
+    {
+        await webRequest.SendWebRequest();
+
+        switch (webRequest.result)
+        {
+            case UnityWebRequest.Result.Success:
+                string responseData = webRequest.downloadHandler.text;
+                Debug.Log("Response: " + responseData);
+
+                // Converteer JSON naar een object als de request een token bevat
+                if (responseData.Contains("accessToken")) // Simpele check om te zien of het een login-response is
+                {
+                    Token tokenData = JsonUtility.FromJson<Token>(responseData);
+                    return new WebRequestData<Token>(tokenData);
+                }
+                return new WebRequestData<string>(responseData);
+
+            default:
+                return new WebRequestError(webRequest.error);
+        }
+    }
+
+
+    private void AddToken(UnityWebRequest webRequest)
+    {
+        webRequest.SetRequestHeader("Authorization", "Bearer " + token);
+    }
+
+    private string RemoveIdFromJson(string json)
+    {
+        return json.Replace("\"id\":\"\",", "");
+    }
+
+}
+
+[Serializable]
+public class Token
+{
+    public string tokenType;
+    public string accessToken;
+}
