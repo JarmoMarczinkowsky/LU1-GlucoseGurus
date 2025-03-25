29d8e859e2edb12f155ad143ab9c5fd9ff3de4d5
Simon-de-Programmeur <simonlaugeman@gmail.com>
2025-03-25 15:56:29 +0100

    AddedPathsAndMangoAnimation
    I added paths and an animation for the mango's. I customized the path for route A and B. The mango's wiggle for about 3 seconds after the application is started

--- Assets/Scripts/TreatmentplanManagerScript.cs
+++ Assets/Scripts/TreatmentplanManagerScript.cs
@@ -11,10 +11,11 @@ using UnityEngine.UI;
 
 public class TreatmentplanManagerScript : MonoBehaviour
 {
-    [Header("Single Objects")]
-    public Slider slider;
-    public TextMeshProUGUI StepCounter;
-    //public GameObject PopUpMenuManager;
+
+    //[Header("Single Objects")]
+    //public Slider slider;
+    //public TextMeshProUGUI StepCounter;
+    ////public GameObject PopUpMenuManager;
 
     [Header("Lists")]
     public List<GameObject> PopUpMenus;
@@ -23,53 +24,69 @@ public class TreatmentplanManagerScript : MonoBehaviour
     //private variables
     private int treatmentStep;
     private int operationStep;
+    private int route;
 
-    void Start()
+    public void SetUp()
     {
         foreach (var menu in PopUpMenus)
         {
             menu.gameObject.SetActive(false);
         }
 
-
-        treatmentStep = (int)slider.value;
-        StepCounter.text = "Stap " + treatmentStep + " voltooid";
-
-        SetColor();
+        treatmentStep = 3;
+        //treatmentStep = (int)slider.value;
+        //StepCounter.text = "Stap " + treatmentStep + " voltooid";
+        SetMango();
 
         operationStep = 3;
 
-        if(treatmentStep == operationStep)
+        if (treatmentStep == operationStep)
         {
             // Show recovery reminders
             // recoveryPopup.gameObject.SetActive(true);
-            // Or something is this direction
+            // Or something in this direction
         }
-
     }
 
     #region TreatmentMoments
-    public void SetColor()
+    public void SetMango()
     {
-        treatmentStep = (int)slider.value;
-        StepCounter.text = "Gebruiker bij stap: " + treatmentStep;
+        //treatmentStep = (int)slider.value;
+        //StepCounter.text = "Gebruiker bij stap: " + treatmentStep;
 
         foreach (var button in TreatmentplanMoments)
         {
-            Color color;
-            ColorUtility.TryParseHtmlString("#969696", out color);
-            button.image.color = color;
+            //Color color;
+            //ColorUtility.TryParseHtmlString("#969696", out color);
+            //button.image.color = color;
+
+            RectTransform rectTransform = button.GetComponent<RectTransform>();
+            Vector2 newSize = new Vector2(130f, 130f);
+            rectTransform.sizeDelta = newSize;
         }
 
         if (treatmentStep >= 0 && treatmentStep <= TreatmentplanMoments.Count)
         {
+            //for (int i = 0; i < treatmentStep; i++)
+            //{
+            //    var button = TreatmentplanMoments[i];
+
+            //    Color color;
+            //    ColorUtility.TryParseHtmlString("#F5F5F5", out color);
+            //    button.image.color = color;
+            //}
+
             for (int i = 0; i < treatmentStep; i++)
             {
                 var button = TreatmentplanMoments[i];
 
-                Color color; 
+                Color color;
                 ColorUtility.TryParseHtmlString("#F5F5F5", out color);
                 button.image.color = color;
+
+                RectTransform rectTransform = button.GetComponent<RectTransform>();
+                Vector2 newSize = new Vector2(200f, 200f);
+                rectTransform.sizeDelta = newSize;
             }
         }
     }
