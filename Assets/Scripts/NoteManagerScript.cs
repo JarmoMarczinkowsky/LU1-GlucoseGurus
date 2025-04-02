771f6ac0be2a146cbf5320ef280153313bf4f835
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2025-04-02 15:44:04 +0200

    Closes note creator on succes

--- Assets/Scripts/NoteManagerScript.cs
+++ Assets/Scripts/NoteManagerScript.cs
@@ -290,6 +290,8 @@ public class NoteManagerScript : MonoBehaviour
             case WebRequestData<Note> dataResponse:
                 newNote.id = dataResponse.Data.id;
                 // TODO: Handle succes scenario.
+                CloseNoteCreator();
+
                 break;
             case WebRequestError errorResponse:
                 string errorMessage = errorResponse.ErrorMessage;
